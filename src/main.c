aba0a27c57f653ae93af1228c0aaf11e22ff6012
Mateus Santo Messias <83839655+butterbr4@users.noreply.github.com>
2022-11-27 15:06:34 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
     Mateus Santos Messias (12548000) - 100%
         Impressão de todos os registros [3]
         Select dos registros [4]
-        Inserção Arvore [9]
+        Inserção Árvore [9]
         Recupera Conectados [10]
 */
 
